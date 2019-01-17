00c1b7a62afc40814977bd3d3749c18ad7563fa4
Osama Akhlaq <35818469+OsamaAkhlaq@users.noreply.github.com>
2019-01-17 17:16:20 +0500

    TextSummarizer(mainClass)\v1.0
    This class only calls the object of the NLP class and hence does nothing and is the main class of the whole program.

---
+++ TextSummarizer.java
@@ -0,0 +1,14 @@
+package textsummarizer;
+import java.io.Reader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class TextSummarizer {
+    public static void main(String[] args)throws IOException {
+       NLP obj = new NLP();
+       obj.MyNewMethod();
+    }
+}
+
